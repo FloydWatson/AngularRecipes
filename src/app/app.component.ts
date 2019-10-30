b2451d96763813eaddd5978c743e01ff95d4284b
FloydWatson <floydbcseit@gmail.com>
2019-10-30 17:17:34 +1300

    added navigation between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // home route
+  loadedFeature = 'recipe';
+
+  // recieveing event from header
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  // allows us to pass this variable to parent component. this will be passed when function called/event emitted
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 
 }
